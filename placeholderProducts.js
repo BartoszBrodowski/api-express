4c3b97431af94e580e3a1d4c55fd746bf9acf406
Bartosz Brodowski <bartolini10058@gmail.com>
2023-01-17 15:56:43 +0100

    Initial setup

---
+++ placeholderProducts.js
@@ -0,0 +1,13 @@
+const products = [
+	{ name: 'Keyboard', price: 100, description: 'A keyboard for your computer', quantity: 1 },
+	{ name: 'Mouse', price: 50, description: 'A mouse for your computer', quantity: 1 },
+	{ name: 'Monitor', price: 200, description: 'A monitor for your computer', quantity: 1 },
+	{ name: 'Headphones', price: 150, description: 'Headphones for your computer', quantity: 1 },
+	{ name: 'Speakers', price: 200, description: 'Speakers for your computer', quantity: 1 },
+	{ name: 'Webcam', price: 100, description: 'A webcam for your computer', quantity: 1 },
+	{ name: 'Printer', price: 200, description: 'A printer for your computer', quantity: 1 },
+	{ name: 'Laptop', price: 1000, description: 'A laptop for your computer', quantity: 1 },
+	{ name: 'Tablet', price: 500, description: 'A tablet for your computer', quantity: 1 },
+	{ name: 'Phone', price: 300, description: 'A phone for your computer', quantity: 1 },
+	{ name: 'Charger', price: 50, description: 'A charger for your computer', quantity: 1 },
+];
